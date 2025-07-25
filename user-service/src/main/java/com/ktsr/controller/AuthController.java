7d237ee09f724c18028c6ba999fe8f57fded117b
kasatejaswarreddy0512 <ktrreddy64@gmail.com>
2025-07-25 19:00:02 +0530

    gateway commit

--- user-service/src/main/java/com/ktsr/controller/AuthController.java
+++ user-service/src/main/java/com/ktsr/controller/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
